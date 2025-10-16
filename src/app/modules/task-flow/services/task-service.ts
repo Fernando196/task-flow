d1d77383971e7209dfe8e2de612b7cd4dc8c47e8
Fernando196 <fer_jose1996@hotmail.com>
2025-10-15 20:08:59 -0500

    feat: implementar TaskService para gestión de tareas
    - Crear servicio con operaciones CRUD completas
    - Implementar gestión de estado con signals
    - Agregar métodos para obtener, crear, actualizar y eliminar tareas
    - Configurar comunicación con API REST
    - Implementar lógica de actualización reactiva del estado

---
+++ src/app/modules/task-flow/services/task-service.ts
@@ -0,0 +1,64 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { firstValueFrom, Observable } from 'rxjs';
+import { API_URL } from '../../../config/config';
+import { ITaskResponse } from '../interfaces/ITaskReponse.interface';
+import { ITaskService } from './iservices/task-service.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService implements ITaskService {
+  tasks = signal<ITask[]>([]);
+
+  constructor(private _http: HttpClient) {}
+
+  completedTasks(id: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((t) => {
+        if (t.id !== id) return t;
+        return {
+          ...t,
+          completed: true,
+        };
+      });
+    });
+  }
+
+  async addTask(task: ITask | null) {
+    return await firstValueFrom(
+      this._http.post<ITaskResponse>(`${API_URL}/tasks`, task)
+    );
+  }
+  async deleteTask(id: number) {
+    return await firstValueFrom(
+      this._http.delete<ITaskResponse>(`${API_URL}/tasks/${id}`)
+    );
+  }
+  async updateTask(id: number, updateTask: ITask) {
+    const completedChange = this.tasks().find((t) => t.id === id);
+    if (
+      completedChange &&
+      JSON.stringify(completedChange) !== JSON.stringify(updateTask)
+    ) {
+      await firstValueFrom(
+        this._http.put<ITaskResponse>(`${API_URL}/tasks/${id}`, updateTask)
+      );
+    }
+
+    this.tasks.update((tasks) => {
+      return tasks.map((t) => {
+        if (t.id !== id) return t;
+        return {
+          ...t,
+          completed: true,
+        };
+      });
+    });
+  }
+  async getTasks() {
+    return await firstValueFrom(
+      this._http.get<ITaskResponse>(`${API_URL}/tasks`)
+    );
+  }
+}

25c649798043ccb9b506d5895a28789c0bc93ff2
Fernando196 <fer_jose1996@hotmail.com>
2025-10-22 23:15:21 -0500

    feat: define ITask interface and add IUser interface for task management

--- src/app/modules/task-flow/interfaces/ITask.interface.ts
+++ src/app/modules/task-flow/interfaces/ITask.interface.ts
@@ -1,6 +1,19 @@
-interface ITask {
+import { IUser } from './IUser.interface';
+
+export interface ITask {
   id: number;
   description: string;
   completed: boolean;
+  startDate: Date;
+  endDate: Date;
   createdDate: Date;
+  statusId: IStatusTask;
+  users?: IUser[];
+}
+
+export enum IStatusTask {
+  TODO = 'TODO',
+  IN_PROGRESS = 'IN_PROGRESS',
+  COMPLETED = 'COMPLETED',
+  IN_REVIEW = 'IN_REVIEW',
 }
---
+++ src/app/modules/task-flow/interfaces/IUser.interface.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  id: number;
+  name: string;
+  thumbnail: string;
+  email?: string;
+}

fd37c5024fd27b94f3c862444abe20645091915d
Fernando196 <fer_jose1996@hotmail.com>
2025-11-04 22:37:52 -0600

    feat: add IconClose component with SVG and update SharedModule for integration

---
+++ src/app/modules/shared/components/icon/icon-close/icon-close.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-close',
+  standalone: false,
+  templateUrl: './icon-close.html',
+  styleUrl: './icon-close.css'
+})
+export class IconClose {
+
+}
--- src/app/modules/shared/shared-module.ts
+++ src/app/modules/shared/shared-module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MapIcon } from './components/icon/map-icon/map-icon';
 import { IconMenu } from './components/icon/icon-menu/icon-menu';
 import { ModalContainer } from './components/modal/modal-container/modal-container';
+import { IconClose } from './components/icon/icon-close/icon-close';
 
 @NgModule({
-  declarations: [MapIcon, IconMenu, ModalContainer],
+  declarations: [MapIcon, IconMenu, ModalContainer, IconClose],
   exports: [MapIcon,ModalContainer],
   imports: [CommonModule],
 })
